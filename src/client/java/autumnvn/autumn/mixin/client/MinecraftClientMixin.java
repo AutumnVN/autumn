54c929004a9fa191e88f9221ca8d7a58ee6dd110
AutumnVN <autumnvnchino@gmail.com>
2024-03-15 10:05:33 +0700

    player inventory while riding

--- src/client/java/autumnvn/autumn/mixin/client/MinecraftClientMixin.java
+++ src/client/java/autumnvn/autumn/mixin/client/MinecraftClientMixin.java
@@ -2,10 +2,16 @@ package autumnvn.autumn.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import autumnvn.autumn.AutumnClient;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.tutorial.TutorialManager;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
@@ -17,5 +23,22 @@ public class MinecraftClientMixin {
     @ModifyConstant(method = "doItemUse", constant = @Constant(intValue = 4))
     private int noUseDelay(int original) {
         return AutumnClient.options.noUseDelay.getValue() ? 0 : original;
+    }
+
+    @Shadow
+    private TutorialManager tutorialManager;
+
+    @Shadow
+    private void setScreen(Screen screen) {}
+
+    // PlayerInventoryWhileRiding
+    @Redirect(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;openRidingInventory()V"))
+    public void openRidingInventory(ClientPlayerEntity player) {
+        if (AutumnClient.client.options.sprintKey.isPressed()) {
+            tutorialManager.onInventoryOpened();
+            setScreen(new InventoryScreen(player));
+        } else {
+            player.openRidingInventory();
+        }
     }
 }
